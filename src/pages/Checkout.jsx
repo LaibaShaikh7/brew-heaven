8dedd30da519ab7de21b81a0025b9516965612b0
Laiba Shaikh <shaikhlaiba764@gmail.com>
2026-09-18 10:40:41 +0500

    Update API endpoint to relative pathFix checkout API URL

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -38,7 +38,7 @@ export default function Checkout() {
   event.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/orders", {
+    const response = await fetch("/api/orders", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
